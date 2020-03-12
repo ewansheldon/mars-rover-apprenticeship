99e20ebe859b5068a0d19008fdf252c66af7cf07
Ewan <ewan.s@cucumberwifi.io>
2020-03-12 09:27:27 +0000

    rover turn left

---
+++ src/main/java/com/codurance/mars_rover/Rover.java
@@ -0,0 +1,10 @@
+package com.codurance.mars_rover;
+
+public class Rover {
+    public Rover(Grid grid) {
+    }
+
+    public String execute(String l) {
+        return "0:0:W";
+    }
+}
--- src/test/java/com/codurance/mars_rover/RoverShould.java
+++ src/test/java/com/codurance/mars_rover/RoverShould.java
@@ -1,4 +1,15 @@
 package com.codurance.mars_rover;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoverShould {
+    @Test
+    void rotate_left() {
+        Grid grid = new Grid(10, 10);
+        Rover rover = new Rover(grid);
+        String output = rover.execute("L");
+        assertEquals("0:0:W", output);
+    }
 }
