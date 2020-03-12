c9cb7a1a1d200c5410ec8aa649507ac1b895032a
Ewan <ewan.s@cucumberwifi.io>
2020-03-12 09:53:29 +0000

    refactor left turn

--- src/main/java/com/codurance/mars_rover/Rover.java
+++ src/main/java/com/codurance/mars_rover/Rover.java
@@ -1,12 +1,28 @@
 package com.codurance.mars_rover;
 
 public class Rover {
+    private String direction;
+
     public Rover(Grid grid) {
+        direction = "N";
     }
 
     public String execute(String input) {
-        if (input.equals("LLL")) return "0:0:E";
-        if (input.equals("LL")) return "0:0:S";
-        return "0:0:W";
+        for (char command : input.toCharArray()) {
+            turnLeft();
+        }
+        return "0:0:" + direction;
+    }
+
+    private void turnLeft() {
+        if (direction.equals("N")) {
+            direction = "W";
+        } else if (direction.equals("W")) {
+            direction = "S";
+        } else if (direction.equals("S")) {
+            direction = "E";
+        } else if (direction.equals("E")) {
+            direction = "N";
+        }
     }
 }
